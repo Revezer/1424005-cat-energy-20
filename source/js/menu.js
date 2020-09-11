3e8bac8de4f3eb155b080f8d6746880c5e90fdd7
Revezer <64742445+Revezer@users.noreply.github.com>
2020-09-11 23:35:32 +0300

    ссылки

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,6 +2,7 @@ var menuLink = document.querySelector(".page-header__toggle");
 var menu = document.querySelector(".main-nav");
 
 menu.classList.remove("main-nav--display");
+menuLink.classList.remove("page-header__toggle--display")
 
 menuLink.addEventListener("click", function (evt) {
     evt.preventDefault();
